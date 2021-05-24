ce3032002a01d801065a52f6ab3de66a58ec7180
Qqkyu <kulmisiu007@gmail.com>
2021-05-24 15:28:24 +0200

    Added function for finding example seats given amount and optional "together" argument

--- src/modules/Seats/index.jsx
+++ src/modules/Seats/index.jsx
@@ -27,20 +27,21 @@ const { Header, Footer, Content } = Layout;
 function Index() {
     const dispatch = useDispatch();
 
+    const seatsLoaded = useSelector((state) => state.availableSeatsLoaded);
+    const seats = useSelector((state) => state.availableSeats);
+
+    const seatAmount = useSelector((state) => state.seatAmount);
+    const seatsTogether = useSelector((state) => state.seatsTogether);
+
     useEffect(() => {
         axios.get(`http://localhost:3000/seats`).then((res) => {
             const seats = res.data;
             const mappedSeats = mapSeatsObject(seats);
             dispatch(setAvailableSeats(mappedSeats));
             dispatch(availableSeatsLoaded(true));
+            console.log(findSeats(mappedSeats[0], 3));
         });
     }, [dispatch]);
-
-    const seatsLoaded = useSelector((state) => state.availableSeatsLoaded);
-    const seats = useSelector((state) => state.availableSeats);
-
-    const seatAmount = useSelector((state) => state.seatAmount);
-    const seatsTogether = useSelector((state) => state.seatsTogether);
 
     return (
         <>
@@ -136,5 +137,108 @@ function mapSeatsObject(seats) {
 
     return seatsArray;
 }
+
+/**
+ * Find given amount of seats.
+ * @param {array} row - Array of seats in single row.
+ * @returns {array} Array of example seats.
+ */
+function findSeats(row, amount, together = false) {
+    let exampleSeats = [];
+    if (together) {
+        const seatsRowAmount = row.length;
+        /* Iterate over possible "first" seats */
+        for (let curSeat = 0; curSeat <= seatsRowAmount - amount; ++curSeat) {
+            /* Push first seat in current iteration to result array */
+            exampleSeats.push(row[curSeat]);
+            /*
+             * Save first seat column in current iteration
+             * Start iterating only if first seat is not reserved - set separated flag to true if first seat is
+             */
+            let [prevSeatCol, separated] = [
+                row[curSeat]["cords"]["y"],
+                row[curSeat]["reserved"],
+            ];
+            for (let i = 1; i < amount && !separated; ++i) {
+                /* Check if current seat is not reserved */
+                if (row[curSeat + i]["reserved"]) {
+                    separated = true;
+                } else {
+                    /* Save current seat's column and compare with previous seat's column */
+                    const curSeatCol = row[curSeat + i]["cords"]["y"];
+                    if (prevSeatCol + 1 !== curSeatCol) {
+                        /* Seats are separated, break the loop */
+                        separated = true;
+                    } else {
+                        /* Seats are next to each other, add seats to result and update prevSeatCol */
+                        exampleSeats.push(row[curSeat + i]);
+                        prevSeatCol = curSeatCol;
+                    }
+                }
+            }
+            /* Check loop result */
+            if (separated) {
+                /* Reset result array */
+                exampleSeats = [];
+            } else {
+                /* Found given amount of seats next to each other - return result */
+                return exampleSeats;
+            }
+        }
+        /* Unable to find amount of given seats next to each other - return empty array */
+        return [];
+    } else {
+        const seatsRowAmount = row.length;
+        /* Iterate over all seats in given row */
+        for (
+            let curSeat = 0;
+            curSeat < seatsRowAmount || exampleSeats.length === amount;
+            ++curSeat
+        ) {
+            const seat = row[curSeat];
+            /* Add seat only if it is not already reserved */
+            if (!seat["reserved"]) {
+                exampleSeats.push(seat);
+            }
+        }
+        /* Return example seats even if it has less seats that given amount */
+        return exampleSeats;
+    }
+}
+
+/*
+        for (let curRow = 0; curRow < rows; ++curRow) {
+            const rowSeats = seats[curRow].length;
+
+            for (let curCol = 0; curCol <= rowSeats - amount; ++curCol) {
+                let [separated, prevSeatCol] = [
+                    false,
+                    seats[curRow][curCol]["cords"]["y"],
+                ];
+
+                exampleSeats.push(seats[curRow][curCol]);
+
+                for (
+                    let curSeat = curCol + 1;
+                    curSeat < curSeat + amount && !separated;
+                    ++curSeat
+                ) {
+                    let curSeatCol = seats[curRow][curSeat]["cords"]["y"];
+
+                    if (prevSeatCol + 1 !== curSeatCol) {
+                        separated = true;
+                    } else {
+                        prevSeatCol = curSeatCol;
+                        exampleSeats.push(seats[curRow][curSeat]);
+                    }
+                }
+                if (separated) {
+                    exampleSeats = [];
+                } else {
+                    return exampleSeats;
+                }
+            }
+        }
+        */
 
 export default Index;
